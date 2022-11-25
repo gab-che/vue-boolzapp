6fd8fdd7df9e73dd6f133ee07fb3e1ca2a753ab0
gab-che <gabriellachessa1@gmail.com>
2022-11-25 13:17:35 +0100

    adds messages dynamically

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ createApp({
     data(){
         return{
             userList,
-            
+            selectedUser: null,
         }
     },
 
     methods: {
 
+    },
+
+    beforeMount(){
+        this.selectedUser = this.userList[0];
     }
 }).mount("#app");
